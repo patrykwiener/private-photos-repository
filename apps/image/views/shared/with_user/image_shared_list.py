f02f0d91aefacf11dc42e0e42ba11233c37aff9e
PWiener <patrykwiener@gmail.com>
2019-12-16 19:09:32 +0100

    fix bug violating users privacy

--- apps/image/views/shared/with_user/image_shared_list.py
+++ apps/image/views/shared/with_user/image_shared_list.py
@@ -7,7 +7,7 @@ class ImageSharedList(ImageSharedListBase):
 
     def create_shared_images(self):
         for image in self.queryset.distinct():
-            for shared_image in image.sharedimagemodel_set.all():
+            for shared_image in image.sharedimagemodel_set.filter(recipient=self.request.user):
                 self.shared_images.setdefault(shared_image.image.user, []).append(image)
 
     def get(self, request, *args, **kwargs):
