507a7a8cf826d92469049178de8f0b337de69c55
PWiener <patrykwiener@gmail.com>
2020-01-02 01:04:18 +0100

    Removed duplicated external libraries.

--- static/js/emoji_picker_manager.js
+++ static/js/emoji_picker_manager.js
@@ -1,7 +1,7 @@
 $(function () {
     window.emojiPicker = new EmojiPicker({
         emojiable_selector: '[data-emojiable=true]',
-        assetsPath: '/static/emoji/img',
+        assetsPath: '/static/external_libs/emoji/img',
         popupButtonClasses: 'fa fa-smile-o'
     });
     window.emojiPicker.discover();
