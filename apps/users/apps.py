cc1ad4a0b43afc166362a19409076d32abe34069
PWiener <patrykwiener@gmail.com>
2020-01-21 23:52:16 +0100

    Users app descriptions.

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,5 +1,7 @@
+"""This module contains ImageConfig class."""
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """Contains users app configuration."""
     name = 'apps.users'
